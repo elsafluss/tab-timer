51bcbd6f32518d4a1b12b1d1c956a75e51b1baf8
Elsa Fluss <elsafluss@gmail.com>
2021-06-07 14:48:05 -0600

    Display final tab duration

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react"
 import Card from "./Card.js"
-import './App.css';
+import "./App.css"
 
 function App() {
-
-  const [tab, setTab] = useState('one')
+  const [tab, setTab] = useState("one")
   const [startTime, setStartTime] = useState(0)
   const [duration, setDuration] = useState(0)
 
   // get the time when the page was first loaded
   useEffect(() => {
     setStartTime(Date.now())
-  }, [])  
-  
+  }, [])
+
   // get the time when a tab is changed (closed)
   // and put the previous tab's duration into state as seconds
   useEffect(() => {
@@ -22,6 +21,13 @@ function App() {
     setDuration(duration)
   }, [tab])
 
+  // in production, this is how I would send the final tab's duration to the back end
+  // useEffect(() => {
+  //   return () => {
+  //     POST duration to back end
+  //   }
+  // }, [])
+
   const chooseTab = () => {
     if (tab === "one") {
       return <Card content="tab1 content" />
@@ -29,18 +35,30 @@ function App() {
       return <Card content="tab2 content" />
     } else if (tab === "three") {
       return <Card content="tab3 content" />
+    } else {
+      return <Card content="you have left the page" />
     }
   }
 
   return (
     <div className="App">
-      <button onClick={() => setTab("one")}>Tab one</button>
-      <button onClick={() => setTab("two")}>Tab two</button>
-      <button onClick={() => setTab("three")}>Tab three</button>
+      {duration < 100000000 ? (
+        <p>that tab was open for {duration} seconds</p>
+      ) : (
+        <p>this tab was opened at {startTime}</p>
+      )}
+      {tab !== "leave" && (
+        <>
+          <button onClick={() => setTab("one")}>Tab one</button>
+          <button onClick={() => setTab("two")}>Tab two</button>
+          <button onClick={() => setTab("three")}>Tab three</button>
+        </>
+      )}
       {chooseTab()}
-      {duration < 100000000 ? <p>the last tab was open for {duration} seconds</p> : <p>this is the first tab opened</p>}
+      <br></br>
+      <button onClick={() => setTab("leave")}>leave the page</button>
     </div>
   )
 }
 
-export default App;
+export default App
--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,6 @@
 import React from "react"
 
 const Card = ({ content }) => {
-    
   return (
     <>
       <p>{content}</p>
